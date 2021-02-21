ff6f4ce2a7ebbc1098e853565050dac4c85bea3a
ind-spirit <error156@yandex.ru>
2021-02-21 19:47:23 +0700

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function reverse (n) {
-  
+    let result,
+    buf,
+    len,
+    str = n.toString(10),
+    arr = str.split('');
+
+if (arr.length % 2 != 0) {
+    len = arr.length - 1;
+} else {
+    len = arr.length;
+}
+
+for (let i = 0; i < len / 2; i++) {
+    buf = arr[i];
+    arr[i] = arr[len - i];
+    arr[len - i] = buf;
+}
+arr = arr.join('');
+
+result = parseInt(arr);
+
+return result;
 }
